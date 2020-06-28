57497b588998de9e3dec477d49271305e518a4a6
DESKTOP-ONPCH2S\lixy <lixianyi@cvnavi.com>
2020-06-28 09:36:31 +0800

    modify health report task

--- task/src/main/java/com/cvnavi/scheduler/task/school/HealthReport.java
+++ task/src/main/java/com/cvnavi/scheduler/task/school/HealthReport.java
@@ -17,9 +17,27 @@ import org.apache.http.util.EntityUtils;
 import org.apache.logging.log4j.Level;
 import org.codehaus.jackson.map.ObjectMapper;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 @ScheduleAnnotation(begin = "06:30:00",end = "06:31:00",period = 600000)
 @Log4j2
 public class HealthReport extends AbstractDailyTask {
+
+    static String authorization="bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoic2hpbmUiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiItMSIsIlVzZXJOYW1lIjoic2hpbmUiLCJVc2VyU24iOiIzODUxM2Q0Ny1iY2ZiLTRmMGMtOTZlOS1kMjgyZGFkYjRmNWYiLCJTY2hvb2xDb2RlIjoiLTEiLCJTY2hvb2xJZCI6IjAiLCJQaG9uZSI6IjE1MDAwMzM3MTIwIiwiZXhwIjoxNTkyMjcxMTc4LCJpc3MiOiJjbnRlY2giLCJhdWQiOiJjbnRlY2gifQ.Ikof0vR_vU9R4fi_-iXbR7aMraCzLTdAtlKqEd4fpwk";
+    static {
+        InputStream is = HealthReport.class.getResourceAsStream("/config.properties");
+        Properties p = new Properties();
+        try {
+            p.load(is);
+            authorization=p.getProperty("health.report.authorization");
+
+        } catch (IOException e) {
+
+        }
+
+    }
 
     @Override
     public void doTask() {
@@ -37,7 +55,7 @@ public class HealthReport extends AbstractDailyTask {
         Header[] headers={
                 new BasicHeader("Accept","application/json, text/plain, */*"),
                 new BasicHeader("Origin","https://xuhui.wechat.chennutech.com"),
-                new BasicHeader("Authorization","bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoic2hpbmUiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiIxNTEwNTAiLCJVc2VyTmFtZSI6InNoaW5lIiwiVXNlclNuIjoiMzg1MTNkNDctYmNmYi00ZjBjLTk2ZTktZDI4MmRhZGI0ZjVmIiwiU2Nob29sQ29kZSI6Ii0xIiwiU2Nob29sSWQiOiIwIiwiUGhvbmUiOiIxNTAwMDMzNzEyMCIsImV4cCI6MTU5MzI2MzIzMCwiaXNzIjoiY250ZWNoIiwiYXVkIjoiY250ZWNoIn0.J7to8CXuPXxrU1d-ts118tp_GTgFjsN_ppJI2W_s02I"),
+                new BasicHeader("Authorization",authorization),
                 new BasicHeader("User-Agent","Mozilla/5.0 (Linux; Android 6.0.1; MI MAX Build/MMB29M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.62 XWEB/2352 MMWEBSDK/200301 Mobile Safari/537.36 MMWEBID/8449 MicroMessenger/7.0.13.1640(0x27000D36) Process/appbrand0 NetType/WIFI Language/zh_CN ABI/arm64 WeChat/arm32 miniProgram"),
                 new BasicHeader("Content-Type","application/json;charset=UTF-8"),
                 new BasicHeader("X-Requested-With","com.tencent.mm"),
